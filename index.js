ec1fb8ced57cbc52c0a041c04de528290e093487
Kimakjun <Kimakjun@naver.com>
2020-08-30 17:17:31 +0900

    jwt 사용하여 login 기능 구현

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+
 const port = 5000;
 
 const config = require('./config/key');
@@ -17,6 +19,7 @@ mongoose.connect(config.mongoURI, {
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -34,6 +37,39 @@ app.post('/register', (req, res) => {
     })
 
 });
+
+app.post('/login', (req, res, next)=>{
+
+    // 요청된 이메일을 데이터 베이스에서 찾는다.
+    // 있다면 비밀번호가 일치하는지 확인.
+    // 비밀번호가 맞다면 토큰 생성하기.
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: '이메일을 확인하세요.'
+            })
+        }
+    
+
+    user.comparePassword(req.body.password, (err, isMatch)=>{
+        if(!isMatch){
+            return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+        }
+        
+        user.generateToken((err, user)=>{
+            if(err) return res.status(400).send(serr);
+
+            res.cookie('user_auth', user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id});
+
+         })
+     })  
+  })
+})
+
+
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +34,51 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// 생성시 new User , 이로직 끝나고 save 로 넘어감.
+// arrow function 에서는 this 를 못찾아서 작동을 안함.
+userSchema.pre('save', function (next){
+    let user = this;
+
+    // 비밀번호가 변경될때만 실행.
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, (err, salt)=> {
+            if(err){
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, (err, hash)=>{
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        })
+    }else{
+        next();
+    }
+
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, (err, isMatch)=>{
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+
+    let user = this;
+
+    const token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save((err, user)=>{
+        if(err) return cb(err);
+        cb(null, user);
+    })
+
+}
 
 // 모델은 스키마를 감싸주는 역활...
 const User = mongoose.model('User', userSchema);
